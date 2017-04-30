37647d5aa8befcbc28585325015ad6cf66eae88e
joh <sojoy82@yahoo.co.jp>
2017-04-30 10:33:24 +0900

    add seach function

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,9 @@
 class Page < ApplicationRecord
+  # include PagesSearchable
+
+  # settings do
+  #   mappings dynamic: 'false' do # 動的にマッピングを生成しない
+  #     indexes :title, analyzer: 'kuromoji', type: 'string'
+  #     indexes :body,  analyzer: 'kuromoji', type: 'string'
+
 end
