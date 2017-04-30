4830e44adad998576bd5637944583c639dc72a66
joh <sojoy82@yahoo.co.jp>
2017-05-01 01:33:12 +0900

    add es func to page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,9 +1,12 @@
 class Page < ApplicationRecord
-  # include PagesSearchable
 
-  # settings do
-  #   mappings dynamic: 'false' do # 動的にマッピングを生成しない
-  #     indexes :title, analyzer: 'kuromoji', type: 'string'
-  #     indexes :body,  analyzer: 'kuromoji', type: 'string'
-
+  include Elasticsearch::Model
+  # インデックスするフィールドの一覧
+  # マッピング情報
+  settings do
+    mappings dynamic: 'false' do # 動的にマッピングを生成しない
+      indexes :title, analyzer: 'kuromoji', type: 'string'
+      indexes :body,  analyzer: 'kuromoji', type: 'string'
+    end
+  end
 end
